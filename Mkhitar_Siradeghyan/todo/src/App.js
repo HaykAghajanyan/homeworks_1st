132ce26f2394b7fafbb065fbbe342f5e0cbdb18a
Mkhitar Siradeghyan <mkhitar.siradeghyan@yandex.ru>
2021-12-19 13:20:07 +0400

    Mkhitar Siradeghyan

--- Mkhitar_Siradeghyan/todo/src/App.js
+++ Mkhitar_Siradeghyan/todo/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
-import TodoList from "./components/TodoList/TodoList";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import TodoList from "./components/TodoList";
 
 export default class App extends Component {
   constructor() {
@@ -11,30 +11,41 @@ export default class App extends Component {
       postList: [],
       searchQuery: "",
       filteredPosts: [],
-      onInputChange: (e) => {
-        this.setState({ searchQuery: e.target.value });
-        let filteredArr = this.state.postList.filter((el) => {
-          return el.title.toLowerCase().includes(this.state.searchQuery);
-        });
-        this.setState({ filteredPosts: filteredArr });
-        console.log(this.state.searchQuery, this.state.filteredPosts);
-      },
     };
   }
+  onQueryChange = (e) => {
+    this.setState({ searchQuery: e.target.value });
+    this.filterArr()
+  };
+  filterArr = () => {
+    let filteredArr = this.state.postList.filter((el) => {
+      return el.title.toLowerCase().includes(this.state.searchQuery);
+    });
+    this.setState({ filteredPosts: filteredArr });
+  }
+
   fetchPosts = () => {
     fetch("https://jsonplaceholder.typicode.com/todos").then((res) => {
       res.json().then((res) => {
-        this.setState({ postList: res });
+        this.setState({ postList: res, filteredPosts: res });
       });
     });
   };
+  componentDidMount(){
+    this.fetchPosts()
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.searchQuery !== prevState.searchQuery) {
+      this.filterArr()
+    }
+  }
   render() {
     return (
       <div className="container">
         <Header />
         <Search
           searchQuery={this.state.searchQuery}
-          onInputChange={this.state.onInputChange}
+          onInputChange={this.onQueryChange}
         />
         <TodoList posts={this.state.filteredPosts} />
       </div>
